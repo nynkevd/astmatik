0bab8694419d3ead239d38693688779f59589ba3
Kevin van den Berge <kevinvandenberge0@gmail.com>
2021-01-15 10:05:02 +0100

    Email template aangepast

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,8 +9,9 @@ const handleEmail = () => {
         // Optional additional arguments
         cc: [], // string or array of email addresses
         bcc: [], // string or array of email addresses
-        subject: 'Rapportage Peakflow',
-        body: 'Some body right here',
+        subject: 'Rapportage Peakflow en Medicatie',
+        body: 'Beste behandelende arts,' + '\n' + '\n' + 'Bijgevoegd vind u het overzicht van de gemeten peakflow en medicatie van de afgelopen week.'
+        + '\n' + '\n' + 'Met vriendelijke groet,' + '\n' + 'Uw naam',
     }).catch(console.error)
 }
 const ContactForm = (props) => {
